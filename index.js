d823f1ce7277189ec891ee7cc74cba28987a21fe
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-14 23:06:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var rank = document.querySelector(".ranked-list li");
   for(var i = 0; i < rank.length; i++) {
-    rank[i].innerHTML = (parseInt(rank[i].innerHTML) + n).toString();
+    rank[i].innerHTML = (parseInt(rank[i].innerText) + n).toString();
   }
 }
 
