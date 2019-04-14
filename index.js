3873f3b8d82603dbfc10cd35f013a8e5b02bdcc1
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-14 23:11:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var rank = document.querySelectorAll(".ranked-list li");
+  for(var i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  return document.querySelector("#grand-node div div div div")
+}
