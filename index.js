fad880aab7612a86be93a8e8382f500e8f3b9c8e
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-14 23:01:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function increaseRankBy(n) {
     rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
   }
 }
+
+function deepestChild() {
+  return document.querySelector("grand-node div div div div")
+}
